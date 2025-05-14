1150af6402cb847056192bcae7e80b3af0630665
Sleepy <sleepysoft@gmail.com>
2025-05-14 13:51:57 +0800

    Refine plugin manager and service engine.

--- playground/service_engine.py
+++ playground/service_engine.py
@@ -1,63 +1,67 @@
-import sys
 import time
 import threading
-import importlib.util
 import traceback
-from pathlib import Path
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+
+from plugin_manager import PluginManager, PluginWrapper, logger
 
 
 class TaskManager:
     def __init__(self, watch_dir: str, security_config=None):
-        self.watch_dir = Path(watch_dir)
+        self.watch_dir = watch_dir
         self.security = security_config     # SecurityConfig()
         self.tasks = {}                     # {file_path: (module_ref, thread, stop_event)}
+        self.plugin_manager = PluginManager(['start_task'])
         self.scan_existing_files()
 
     def scan_existing_files(self):
-        for py_file in self.watch_dir.glob("*.py"):
-            if py_file.is_file():
-                self.add_task(str(py_file))
+        plugins = self.plugin_manager.scan_path(self.watch_dir)
+        for plugin in plugins:
+            self.__add_module(plugin)
 
-    def add_task(self, file_path):
-        # 动态加载模块
-        module_name = f"dynamic_{Path(file_path).stem}_{time.time()}"
-        spec = importlib.util.spec_from_file_location(module_name, file_path)
-        module = importlib.util.module_from_spec(spec)
-        sys.modules[module_name] = module
-        spec.loader.exec_module(module)
-        
-        # 验证接口函数
-        if not (hasattr(module, 'start_task') and callable(module.start_task)):
+    def add_task(self, file_path: str):
+        plugin = self.plugin_manager.add_plugin(file_path)
+
+        if not plugin or not plugin.has_function('start_task'):
+            logger.error('Load plugin {file_path} fail.')
             return False
-        
-        # 创建停止事件和线程
+
+        return self.__add_module(plugin)
+    
+    def remove_task(self, file_path):
+        plugin_name = PluginManager.plugin_name(file_path)
+        if plugin_name not in self.tasks:
+            return
+
+        module, thread, stop_event = self.tasks[file_path]
+        stop_event.set()
+
+        thread.join(timeout=30)
+
+        del self.tasks[file_path]
+        self.plugin_manager.remove_plugin(plugin_name)
+
+    def __add_module(self, plugin: PluginWrapper) -> bool:
+        # module_name = f"dynamic_{Path(file_path).stem}_{time.time()}"
+        # spec = importlib.util.spec_from_file_location(module_name, file_path)
+        # module = importlib.util.module_from_spec(spec)
+        # sys.modules[module_name] = module
+        # spec.loader.exec_module(module)
+
+        if not plugin.has_function('start_task'):
+            return False
+
         stop_event = threading.Event()
         thread = threading.Thread(
-            target=module.start_task, 
+            target=plugin.start_task,
             args=(stop_event,),
             daemon=True
         )
         thread.start()
-        
-        self.tasks[file_path] = (module, thread, stop_event)
+
+        self.tasks[plugin.plugin_name] = (plugin, thread, stop_event)
         return True
-    
-    def remove_task(self, file_path):
-        if file_path not in self.tasks:
-            return
-        
-        # 发送停止信号
-        module, thread, stop_event = self.tasks[file_path]
-        stop_event.set()
-        
-        # 等待任务完成
-        thread.join(timeout=30)
-        
-        # 清理资源
-        del sys.modules[module.__name__]
-        del self.tasks[file_path]
 
     # def verify_security(self, file_path):
     #     # 哈希白名单校验
