23da107c4024e58fcc295616f4dde616a5667dae
Sleepy <sleepysoft@gmail.com>
2025-05-18 15:18:53 +0800

    Make crawl task as functional programming paradigm.

--- CrawlTasks/task_crawl_chinanews.py
+++ CrawlTasks/task_crawl_chinanews.py
@@ -1,6 +1,7 @@
 import logging
 import time
 import traceback
+from functools import partial
 from typing import Dict
 
 import Scraper.RequestsScraper
@@ -12,7 +13,7 @@ from Scraper.PlaywrightRenderedScraper import fetch_content
 from Scrubber.HTMLConvertor import html_content_converter
 from Scrubber.UnicodeSanitizer import sanitize_unicode_string
 from Streamer.ToFileAndHistory import to_file_and_history
-
+from Workflow.CommonFeedsCrawFlow import feeds_craw_flow
 
 feed_list = {
     "即时新闻": "https://www.chinanews.com.cn/rss/scroll-news.xml",
@@ -28,51 +29,61 @@ def module_init(service_context):
 
 
 def start_task(stop_event):
-    for feed_name, feed_url in feed_list.items():
-        if stop_event.is_set():
-            break
-        try:
-            print(f'Process feed: {feed_name} : {feed_url}')
-            result = fetch_feed(feed_url, Scraper.RequestsScraper, {})
+    feeds_craw_flow('chinanews', feed_list, stop_event, 15 * 60,
+                    partial(fetch_feed, scraper=Scraper.RequestsScraper, proxy={}),
+                    partial(fetch_content, timeout_ms=20 * 1000),
+                    [
+                        partial(html_content_converter, selector='div.left_zw'),
+                        partial(sanitize_unicode_string, max_length=10240)
+                    ])
 
-            for article in result['entries']:
-                article_link = article['link']
 
-                if ContentHistory.has_url(article_link):
-                    continue
-
-                print(f'|__Fetch article: {article_link}')
-                content = fetch_content(article_link, 20 * 1000)
-
-                raw_html = content['content']
-                if not raw_html:
-                    logging.error('  |__Got empty HTML content.')
-                    continue
-
-                # TODO: If an article always convert fail. Need a special treatment.
-
-                markdown = html_content_converter(raw_html, 'div.left_zw')
-                if not markdown:
-                    logging.error('  |__Got empty content when converting to markdown.')
-                    continue
-
-                clean_text = sanitize_unicode_string(markdown, max_length = 10000)
-                if not clean_text:
-                    logging.error('  |__Got empty content when sanitizing unicode string.')
-                    continue
-
-                success, file_path = to_file_and_history(article_link, clean_text, article['title'], feed_name, '.md')
-                if not success:
-                    logging.error(f'  |__Save content {file_path} fail.')
-                    continue
-
-        except Exception as e:
-            print(f"Process feed fail: {feed_url} - {str(e)}")
-            print(traceback.format_exc())
-
-    # Wait 10 minutes for next loop and check event per 5s.
-    # noinspection PyTypeChecker
-    for _ in range(10 * 60 // 5):
-        if stop_event.is_set():
-            break
-        time.sleep(5)
+# def start_task(stop_event):
+#     for feed_name, feed_url in feed_list.items():
+#         if stop_event.is_set():
+#             break
+#         try:
+#             print(f'Process feed: {feed_name} : {feed_url}')
+#             result = fetch_feed(feed_url, Scraper.RequestsScraper, {})
+#
+#             for article in result['entries']:
+#                 article_link = article['link']
+#
+#                 if ContentHistory.has_url(article_link):
+#                     continue
+#
+#                 print(f'|__Fetch article: {article_link}')
+#                 content = fetch_content(article_link, 20 * 1000)
+#
+#                 raw_html = content['content']
+#                 if not raw_html:
+#                     logging.error('  |__Got empty HTML content.')
+#                     continue
+#
+#                 # TODO: If an article always convert fail. Need a special treatment.
+#
+#                 markdown = html_content_converter(raw_html, 'div.left_zw')
+#                 if not markdown:
+#                     logging.error('  |__Got empty content when converting to markdown.')
+#                     continue
+#
+#                 clean_text = sanitize_unicode_string(markdown, max_length = 10000)
+#                 if not clean_text:
+#                     logging.error('  |__Got empty content when sanitizing unicode string.')
+#                     continue
+#
+#                 success, file_path = to_file_and_history(article_link, clean_text, article['title'], feed_name, '.md')
+#                 if not success:
+#                     logging.error(f'  |__Save content {file_path} fail.')
+#                     continue
+#
+#         except Exception as e:
+#             print(f"Process feed fail: {feed_url} - {str(e)}")
+#             print(traceback.format_exc())
+#
+#     # Wait 10 minutes for next loop and check event per 5s.
+#     # noinspection PyTypeChecker
+#     for _ in range(10 * 60 // 5):
+#         if stop_event.is_set():
+#             break
+#         time.sleep(5)
---
+++ Workflow/CommonFeedsCrawFlow.py
@@ -0,0 +1,100 @@
+import time
+import logging
+import traceback
+import threading
+from typing import Callable, TypedDict, Dict, List
+
+from Streamer.ToFileAndHistory import to_file_and_history
+from Tools.ContentHistory import has_url
+
+
+class FetchFeedEntries(TypedDict):
+    link: str
+    title: str
+
+
+class FetchFeedResult(TypedDict):
+    entries: List[FetchFeedEntries]
+
+
+class FetchContentResult(TypedDict):
+    content: str
+
+
+
+def feeds_craw_flow(flow_name: str, feeds: Dict[str, str], stop_event: threading.Event, update_interval_s: int,
+                    fetch_feed: Callable[[str], FetchFeedResult],
+                    fetch_content: Callable[[str, int], FetchContentResult],
+                    scrubbers: List[Callable[[str], str]]):
+    """
+    A common feeds and their articles craw workflow. This workflow works in this sequence:
+        fetch_feed -> for each feed: fetch_content -> apply scrubbers
+
+    :param flow_name: The workflow name for logging and tracing.
+    :param feeds: The feeds dict like:
+                {
+                    'feed name': 'feed link'
+                }
+    :param stop_event: The stop event to quit loop.
+    :param update_interval_s: The polling udpate interval in second.
+
+    :param fetch_feed: The function to fetch feed. Function declaration:
+                        fetch_feed(feed_url: str) -> dict
+    :param fetch_content: The function to fetch web content by url. Function declaration:
+                        fetch_content(article_link: str) -> dict
+    :param scrubbers: The functions to process scrubbed text. Function declaration:
+                        scrubber(text: str) -> str
+
+    :return: None
+    """
+    logging.info(f'[{flow_name}]: starts work.')
+
+    for feed_name, feed_url in feeds.items():
+        if stop_event.is_set():
+            break
+        try:
+            print(f'Process feed: {feed_name} : {feed_url}')
+            result = fetch_feed(feed_url)
+
+            for article in result['entries']:
+                article_link = article['link']
+
+                if has_url(article_link):
+                    continue
+
+                print(f'|__Fetch article: {article_link}')
+                content = fetch_content(article_link)
+
+                raw_html = content['content']
+                if not raw_html:
+                    logging.error('  |__Got empty HTML content.')
+                    continue
+
+                # TODO: If an article always convert fail. Need a special treatment.
+
+                text = raw_html
+                for scrubber in scrubbers:
+                    text = scrubber(text)
+                    if not text:
+                        logging.error(f'  |__Got empty content when applying scrubber {str(scrubber)}.')
+                        continue
+
+                success, file_path = to_file_and_history(article_link, text, article['title'], feed_name, '.md')
+                if not success:
+                    logging.error(f'  |__Save content {file_path} fail.')
+                    continue
+
+        except Exception as e:
+            print(f"Process feed fail: {feed_url} - {str(e)}")
+            print(traceback.format_exc())
+
+    logging.info(f'[{flow_name}]: Finished one loop and rest for {update_interval_s} seconds ...')
+
+    # Wait for next loop and check event per 5s.
+    # noinspection PyTypeChecker
+    for _ in range(update_interval_s // 5):
+        if stop_event.is_set():
+            break
+        time.sleep(5)
+
+
